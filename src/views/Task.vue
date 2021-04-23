e055cb619080273e6904d2bdb09f612f744d5398
Patryk Ptak <ptaszqq@gmail.com>
2021-04-23 20:52:06 +0100

    Task component

--- src/views/Task.vue
+++ src/views/Task.vue
@@ -1,17 +1,31 @@
 <template>
   <div class="task-view">
-
+    <div class="flex flex-col flex-grow items-start justify-between px-4">
+      {{ task.name }}
+    </div>
+    <textarea
+      class="relative bg-transparent px-2 border mt-2 h-24 mx-2 border-none leading-normal"
+      :value="task.description"
+    />
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
+  computed: {
+    ...mapGetters(['getTask']),
+    task() {
+      return this.getTask(this.$route.params.id)
+    }
+  }
 }
 </script>
 
 <style>
 .task-view {
-  @apply relative flex flex-row bg-white pin mx-4 m-32 mx-auto py-4 text-left rounded shadow;
+  @apply relative flex flex-col bg-white pin mx-4 m-32 mx-auto py-4 text-left rounded shadow;
   max-width: 700px;
 }
 </style>
